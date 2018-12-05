635acd946c7a60176f50456da1f2378ecee3ce43
Lukas Skala <lukas.skala@infor.com>
2018-12-05 07:14:03 +0100

    day5, change rule to allow generate methods

--- src/advent2018/README.md
+++ src/advent2018/README.md
@@ -2,5 +2,5 @@
 
 Three simple rules for this year:
 * No for and while cycles or forEach methods allowed.
-* Every method starts with a return statement (with exception of main, reducer methods and CheckedWrapper class).
+* Every method starts with a return statement (with exception of main, reducer and generate methods and CheckedWrapper class).
 * No method has more than two indents while staying readable (keep the lines short and easy to read).
---
+++ src/advent2018/day5/Main.java
@@ -0,0 +1,82 @@
+package advent2018.day5;
+
+import util.InputLoader;
+
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        String input = InputLoader.loadInput().get(0);
+
+        System.out.println(part1(new Reducer(input)));
+        System.out.println(part2(input));
+    }
+
+    private static int part2(String input) {
+        return IntStream.range('a', 'z'+1)
+                .map(i -> part1(new Reducer(replace(input, i))))
+                .min()
+                .orElseThrow(IllegalArgumentException::new);
+    }
+
+    private static String replace(String s, int c) {
+        return s.replace(Character.toString((char)c), "").replace(Character.toString((char)(c - 32)), "");
+    }
+
+    private static int part1(Reducer reducer) {
+        return Stream.generate(reducer::generate)
+                .flatMap(IntStream::boxed)
+                .map(reducer::process)
+                .filter(Reducer::isFinished)
+                .findFirst()
+                .map(Reducer::result)
+                .orElseThrow(IllegalArgumentException::new);
+    }
+
+    private static class Reducer {
+        private char[] currentString;
+        private StringBuilder newString;
+        boolean replaced;
+        boolean skipNext;
+        int position;
+
+        public Reducer(String s) {
+            newString = new StringBuilder().append(s);
+        }
+
+        IntStream generate() {
+            replaced = false;
+            skipNext = false;
+            currentString = newString.toString().toCharArray();
+            newString = new StringBuilder();
+            return IntStream.range(0, currentString.length);
+        }
+
+        Reducer process(int index) {
+            position = index;
+            if (skipNext) {
+                skipNext = false;
+            } else {
+                if (index != currentString.length-1) {
+                    skipNext = Math.abs(currentString[index] - currentString[index + 1]) == 32;
+                    replaced = skipNext || replaced;
+                }
+                if (!skipNext)
+                    newString.append(currentString[index]);
+            }
+            return this;
+        }
+
+        boolean isFinished() {
+            return position == currentString.length - 2 && !replaced;
+        }
+
+        int result() {
+            return currentString.length;
+        }
+
+    }
+
+}

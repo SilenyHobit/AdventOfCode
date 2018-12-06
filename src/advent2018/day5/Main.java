ec973770a6f23bdb5a96f39d2ee3e9dc261aea6e
Lukas Skala <lukas.skala@infor.com>
2018-12-06 11:45:15 +0100

    day 5 optimization

--- src/advent2018/day5/Main.java
+++ src/advent2018/day5/Main.java
@@ -3,6 +3,7 @@ package advent2018.day5;
 import util.InputLoader;
 import util.Printer;
 
+import java.util.Comparator;
 import java.util.Optional;
 import java.util.stream.IntStream;
 
@@ -10,14 +11,15 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
         Optional.of(InputLoader.loadInput().get(0))
-                .map(line -> Printer.print(part1(new StringBuilder(line)), line))
+                .map(line -> part1(new StringBuilder(line)))
+                .map(line -> Printer.print(line.length(), line))
                 .map(line -> Printer.print(part2(line), line))
                 .orElseThrow(RuntimeException::new);
     }
 
     private static int part2(String input) {
         return IntStream.range('a', 'z'+1)
-                .map(i -> part1(new StringBuilder(replace(input, i))))
+                .map(i -> part1(new StringBuilder(replace(input, i))).length())
                 .min()
                 .orElseThrow(IllegalArgumentException::new);
     }
@@ -26,15 +28,15 @@ public class Main {
         return s.replace(Character.toString((char)c), "").replace(Character.toString((char)(c - 32)), "");
     }
 
-    private static int part1(StringBuilder input) {
+    private static String part1(StringBuilder input) {
         return IntStream.range(-(input.length()-1), 0)
                 .map(i -> -i)
                 .filter(i -> i < input.length())
                 .filter(i -> Math.abs(input.charAt(i-1) - input.charAt(i)) == 32)
                 .mapToObj(i -> input.delete(i-1, i+1))
-                .map(builder -> builder.length())
-                .sorted()
+                .sorted(Comparator.comparingInt(builder -> builder.length()))
                 .findFirst()
-                .orElseThrow(IllegalArgumentException::new);
+                .map(StringBuilder::toString)
+                .orElse(input.toString());
     }
 }

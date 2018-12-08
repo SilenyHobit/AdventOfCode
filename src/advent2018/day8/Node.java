61b23b3df5512bcb6c3b04b3bce97eaf8ea6d0da
Lukas Skala <indy@hobitin.net>
2018-12-08 09:26:32 +0100

    fix Node co coply with rules

--- src/advent2018/day8/Node.java
+++ src/advent2018/day8/Node.java
@@ -2,6 +2,7 @@ package advent2018.day8;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 class Node {
 
@@ -11,21 +12,23 @@ class Node {
     int nodesRemaining;
     int metadataRemaining = -1;
 
-    public Node(Node parent) {
+    Node(Node parent) {
         this.parent = parent;
         this.metadata = new ArrayList<>();
         this.nodes = new ArrayList<>();
     }
 
     int value() {
-        if (nodes.isEmpty())
-            return metadata.stream().mapToInt(i -> i).sum();
-
         return metadata.stream()
-                .map(i -> i-1)
+                .mapToInt(i -> nodes.isEmpty() ? i : childValue(i-1))
+                .sum();
+    }
+
+    private int childValue(int index) {
+        return Optional.of(index)
                 .filter(i -> i < nodes.size())
                 .map(nodes::get)
-                .mapToInt(Node::value)
-                .sum();
+                .map(Node::value)
+                .orElse(0);
     }
 }
